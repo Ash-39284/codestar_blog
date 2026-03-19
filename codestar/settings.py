e830b55971dddc508ad91c53016fd5f0da6a4fcd
Ash-39284 <ashroberts98@icloud.com>
2026-03-19 22:07:11 +0000

    Add second secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-j%3zq4jz68pv%i&&flo^_wk^lho0(acjkfk%md6!ot$0h)qowq'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -86,9 +87,10 @@ DATABASES = {
 }
 
 DATABASES = {
-    'default':
-    dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
 
 
 # Password validation
