89d040882aac17d24d6e055abed4ed8012462b0b
Ash-39284 <ashroberts98@icloud.com>
2026-04-17 14:12:24 +0100

    Add profile image model

--- about/models.py
+++ about/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django_summernote.fields import SummernoteTextField
+from cloudinary.models import CloudinaryField
 
 class About(models.Model):
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.CharField(max_length=1500)
 
@@ -17,3 +19,4 @@ class CollaborateRequest(models.Model):
 
     def __str__(self):
         return f"Collaboration request from {self.name}"
+
